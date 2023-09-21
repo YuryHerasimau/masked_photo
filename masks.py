5e362aca161d09f4944fe8536fd6e7cc1dd89d20
Yury Herasimau <yl.herasimau@gmail.com>
2023-09-21 14:01:31 +0300

    Made masks on a black background instead of a transparent. Cut off the Ignore class instead of painting it black

--- masks.py
+++ masks.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw
 import xml.etree.ElementTree as ET
 
+
 xml_file = "Задание2/masks.xml"
 photos_folder = "Задание2/images/"
 
@@ -16,9 +17,9 @@ for image in root.iter('image'):
     image_width = int(image.get('width'))
     image_height = int(image.get('height'))
 
-    # Создание пустой маски на черном (?) фоне
-    mask = Image.new('RGBA', (image_width, image_height), (0, 0, 0, 0))
-    draw = ImageDraw.Draw(mask)
+    # Создание пустых масок с прозрачным и черным фоном
+    transparent_image = Image.new("RGBA", (image_width, image_height), (0, 0, 0, 0))
+    black_image = Image.new("RGB", (image_width, image_height), (0, 0, 0))
 
     # Проход по каждому элементу polygon внутри image
     for polygon in image.iter('polygon'):
@@ -29,26 +30,31 @@ for image in root.iter('image'):
         # Разделение точек на координаты
         points = [tuple(map(float, point.split(','))) for point in points.split(';')]
 
-        # Отрисовка полигона на маске
-        if label == 'Ignore':
-            color = 'black' # color = (0, 0, 0, 255)
-        else:
-            color = 'fuchsia'
+        # Создаем объект ImageDraw.Draw для рисования маски
+        draw_transparent = ImageDraw.Draw(transparent_image)
+        draw_black = ImageDraw.Draw(black_image)
 
-        draw.polygon(points, fill=color)
+        # Отрисовка полигона на маске. Если тип маски "Ignore", пропускаем этот элемент
+        if label == "Ignore":
+            continue
+        if label == "Skin":
+            color = 'fuchsia'
+        # Если тип маски "Skin", рисуем маску на прозрачном и на черном фоне
+        draw_black.polygon(points, fill=color)
+        draw_transparent.polygon(points, fill=color)
 
     # Загрузка оригинального изображения
     photo_path = photos_folder + image_name.split('/')[-1]
     print('photo_path :', photo_path)
     photo = Image.open(photo_path)
 
-    # Наложение маски на оригинал
-    masked_photo = Image.alpha_composite(photo.convert('RGBA'), mask.convert('RGBA'))
+    # Наложение прозрачной маски на оригинал
+    masked_photo = Image.alpha_composite(photo.convert('RGBA'), transparent_image.convert('RGBA'))
 
-    # Сохранение маски на черном (?) фоне
+    # Сохранение маски на черном фоне
     mask_path = photos_folder + "mask_" + image_id + ".png"
     print('mask_path :', mask_path)
-    mask.save(mask_path)
+    black_image.save(mask_path)
 
     # Сохранение наложенной маски на оригинал
     masked_photo_path = photos_folder + "masked_photo_" + image_id + ".png"
